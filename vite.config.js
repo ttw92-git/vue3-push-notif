be6bdaf22362a324111aebb787dfbacad91aadb7
unknown <tantahwe@gmail.com>
2022-06-06 20:04:48 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -9,45 +9,42 @@ export default defineConfig({
   plugins: [
     vue(), 
     VitePWA({
-      // mode: "development",
-      // base: "/",
-      // srcDir: "src",
-      // filename: "sw.ts",
-      // includeAssets: ["/favicon.png"],
-      // strategies: "injectManifest",
-      // manifest: {
-      //   name: "Test Project",
-      //   short_name: "Test",
-      //   theme_color: "#ffffff",
-      //   start_url: "/",
-      //   display: "standalone",
-      //   background_color: "#ffffff",
-      //   icons: [
-      //     {
-      //       src: "icon-192.png",
-      //       sizes: "192x192",
-      //       type: "image/png",
-      //     },
-      //     {
-      //       src: "/icon-512.png",
-      //       sizes: "512x512",
-      //       type: "image/png",
-      //     },
-      //     {
-      //       src: "icon-512.png",
-      //       sizes: "512x512",
-      //       type: "image/png",
-      //       purpose: "any maskable",
-      //     },
-      //   ],
-      // },
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      }
     })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: process.env.PORT || 4002,
   }
 })
